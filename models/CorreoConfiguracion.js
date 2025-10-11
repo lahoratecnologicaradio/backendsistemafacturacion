b66515ee25c8c952616a8a5f19739fb2262ca7ee
lahoratecnologicaradio <lahoratecnologicaradio@gmail.com>
2025-10-11 18:56:17 -0400

    Update CorreoConfiguracion.js

--- models/CorreoConfiguracion.js
+++ models/CorreoConfiguracion.js
@@ -1,1 +1,16 @@
 
+'use strict';
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../db');
+
+// Tabla sugerida: correo_configuracion (id, correo)
+// Si tu tabla se llama distinto, ajusta "tableName".
+const CorreoConfiguracion = sequelize.define('CorreoConfiguracion', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  correo: { type: DataTypes.STRING(190), allowNull: false }
+}, {
+  tableName: 'correo_configuracion',
+  timestamps: false
+});
+
+module.exports = CorreoConfiguracion;
